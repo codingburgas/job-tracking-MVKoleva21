403c56572c29047a2b4d2da756979535d0733de4
MVKoleva21 <MVKoleva21@codingburgas.bg>
2025-07-02 06:49:39 +0300

    Implement additional filters etc

---
+++ API/JobTracking.DataAccess/Base/IJobPosting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracking.DataAccess.Data.Base
+{
+    public enum PositionStatus
+    {
+        Active,
+        Inactive
+    }
+    public interface IPositionPosting
+    {
+        int Id { get; set; }
+
+        string Title { get; set; }
+        string CompanyName { get; set; }
+        string Description { get; set; }
+        DateTime DatePosted { get; set; }
+        PositionStatus Status { get; set; }
+    }
+
+}
---
+++ API/JobTracking.DataAccess/Base/IUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracking.DataAccess.Data.Base
+{
+    public enum UserRole
+    {
+        User,
+        Admin
+    }
+    public interface IUser
+    {
+        int Id { get; set; }
+
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+        string LastName { get; set; }
+        string Username { get; set; }
+        string PasswordHash { get; set; }
+
+        UserRole Role { get; set; }
+    }
+}
